cede263167e74c5e8b5138bbbf6bccb2141745eb
3evv <3evvsky@gmail.com>
2024-07-09 23:12:16 +0200

    1x glueing done

--- GUIscope.py
+++ GUIscope.py
@@ -1,14 +1,15 @@
 import glob,os,sys
 import cv2 
 import math
+import cv2
 import numpy as np
 import serial
 import time
-
+from stitching import stitch 
 scannedlist = []
 scanningPlan = []
 os.chdir("C:/Users/kamci/Desktop/Microwizard_software/micoscope_code")
-#call for taking 1x magingication
+#call for taking 1x magnification
 
 cam_port = 0
 cam = cv2.VideoCapture(cam_port, cv2.CAP_DSHOW) 
@@ -31,7 +32,7 @@ def take1Xscan():
         # ser.port = '/dev/ttyACM0'
         ser.port = 'COM3'
         ser.baudrate = 9600
-        ser.timeout=0.1
+        ser.timeout=None
         #ser.parity=serial.PARITY_EVEN
         #ser.rtscts=1
         ser.open()
@@ -44,14 +45,16 @@ def take1Xscan():
         #             print(output)
         #         output = ser.readline().decode().strip()
         #     print(output)
-     
-        def waitforcommand(command):
+        
+        def waitforcommand(input):
             output = ser.readline().decode().strip()
-            while output != command :
+            while output != input :
                 if output:
                     print(output)
                 output = ser.readline().decode().strip()
                 time.sleep(0.1)
+            print("Waiting for command: done")
+            return input
 
         #ser.write(bytes("M114", 'utf-8'))
         #tmp = ser.readline().decode().strip()
@@ -62,42 +65,138 @@ def take1Xscan():
         #     print("Zeroed X & Y")   
         # else:
         #     print("X & Y already zeroed")       
-        command = "G0 X: " + str(-120000) + " Y: " + str(-200000) + " Z: " + str(0) + " ;"
+        command = "G0 X: " + str(-150000) + " Y: " + str(-200000) + " Z: " + str(0) + " ;"
         ser.write(bytes(command, 'utf-8'))
         waitforcommand(command + " Done")
+        print("reading camera")
+        time.sleep(1)
         readCamera("Right")
-        command = "G0 X: " + str(-40000) + " Y: " + str(-200000) + " Z: " + str(0) + " ;"
+        command = "G0 X: " + str(-20000) + " Y: " + str(-200000) + " Z: " + str(0) + " ;"
         ser.write(bytes(command, 'utf-8'))
         waitforcommand(command + " Done")
+        time.sleep(1)
         readCamera("Left")
+def fadeIn (img1, img2): #pass images here to fade between
+        #while True:
+        for IN in range(0,10):
+                fadein = IN/10.0
+                dst = cv2.addWeighted( img1, fadein, img2, fadein, 0)#linear $
+                cv2.imshow('window', dst)
+                cv2.waitKey(1)
+                time.sleep(0.05)
+                if fadein == 1.0: #blendmode mover
+                        fadein = 1.0
+        return # exit function
+
 
 def Glue1xMag():
+    # take1Xscan()
     Left1x=cv2.imread("data/Left.jpg", cv2.IMREAD_ANYCOLOR) 
     Right1x=cv2.imread("data/Right.jpg", cv2.IMREAD_ANYCOLOR) 
     Left1x = cv2.undistort(Left1x, mtx, dist, None, newcameramtx)
     Right1x = cv2.undistort(Right1x, mtx, dist, None, newcameramtx)
+    # Left1x =  cv2.equalizeHist(Left1x)
+    # Right1x = cv2.equalizeHist(Right1x)
+    # crop the image
+    h, w = Left1x.shape[:2]
+    x, y, w, h = roi
+    Left1x = Left1x[y:y+h, x:x+w]
+    h, w = Right1x.shape[:2]
+    x, y, w, h = roi
+    Right1x = Right1x[y:y+h, x:x+w]
     cv2.imwrite( "undistorted left.jpg", Left1x)
     cv2.imwrite( "undistorted right.jpg", Right1x)
-    glueing_x = 486
-    cut_line_x = 1160 - glueing_x # 1184
-    glueing_y = 15
-    glue_alpha=0.5
+    glueing_x = 634
+    cut_line_x = 1024 - glueing_x # 1184
+    glueing_y = 0
+    glue_alpha=0.75
     fill_color = (0,0,0)
     #extend left to match right after resize
-    left_filler = np.full((Left1x.shape[0] + glueing_y,Left1x.shape[1] + glueing_x, Left1x.shape[2]), fill_color, dtype=Left1x.dtype)
-    left_filler[5:5+Left1x.shape[0], 0:Left1x.shape[1]] = Left1x
-    Left1x = left_filler.copy()
-    
+    filler = np.full((Left1x.shape[0] + glueing_y,Left1x.shape[1] + glueing_x, Left1x.shape[2]), fill_color, dtype=Left1x.dtype)
+    filler[glueing_y:glueing_y+Left1x.shape[0], 0:Left1x.shape[1]] = Left1x[0:,0:]
+    # Left1x = left_filler.copy()
     #extend right to match left after resize
-
-    right_filler = np.full((Right1x.shape[0] + glueing_y,Right1x.shape[1] + glueing_x, Right1x.shape[2]), fill_color, dtype=Right1x.dtype)
-    right_filler[0:Right1x.shape[0], glueing_x + cut_line_x:] = Right1x[:, cut_line_x:]
-    Right1x = right_filler.copy()
+    # right_filler = np.full((Right1x.shape[0] + glueing_y,Right1x.shape[1] + glueing_x, Right1x.shape[2]), fill_color, dtype=Right1x.dtype)
+    filler[0:Right1x.shape[0], glueing_x + cut_line_x:] = Right1x[:, cut_line_x:]
+    # Right1x = right_filler.copy()
     # print(Right1x.shape, Left1x.shape)
     # return Left1x
-    stitched = cv2.addWeighted(Left1x, glue_alpha, Right1x, 1-glue_alpha, 0)
-    cv2.imwrite( "stitched.jpg", stitched)
-    return stitched
+    # Left1x = cv2.resize(Left1x, (Right1x.shape[0],Right1x.shape[1]))
+    Left_fade = Left1x[:,glueing_x:]
+    Left_alpha = cv2.cvtColor(Left_fade, cv2.COLOR_RGB2RGBA)
+    alpha_mask_left = np.zeros([Left_fade.shape[0],Left_fade.shape[1]])
+    # print(alpha_mask_left)
+    alpha_mask_coefficient = 255
+    for i in range(0,Left_fade.shape[1] - 1):
+        # print(i, Left_fade.shape[0] - 1)
+        for x in range(0,Left_fade.shape[0] - 1):
+            
+            alpha_mask_left[Left_fade.shape[0] - 1 - x][Left_fade.shape[1] - 1 - i] = round(alpha_mask_coefficient * float(i / Left_fade.shape[1]))  
+            
+
+    Left_alpha[:, :, 3] = alpha_mask_left
+    # print(alpha_mask_left[:][100], round(255 * float(100 / Left_fade.shape[0])))
+
+    Right_fade = Right1x[:, :Right1x.shape[1] - glueing_x]
+    Right_alpha = cv2.cvtColor(Right_fade, cv2.COLOR_RGB2RGBA)
+    alpha_mask_right = np.zeros([Right_alpha.shape[0],Right_alpha.shape[1]])
+    # print(alpha_mask_right)
+    for i in range(0,Right_fade.shape[1] - 1):
+        # print(i, Right_fade.shape[0] - 1)
+        for x in range(0,Right_fade.shape[0] - 1):
+            alpha_mask_right[x][i] = round(alpha_mask_coefficient * (i / Right_fade.shape[0]))
+    Right_alpha[:, :, 3] = alpha_mask_right
+
+#   cv2.addWeighted(Right_alpha, glue_alpha, Left_alpha, 1-glue_alpha, 0)
+    # fadeIn (Right_alpha, Left_alpha)
+
+        # get image dimensions
+    imgHeight, imgWidth = Right_alpha.shape[:2]
+
+    # create empty overlay layer with 4 channels
+    overlay = np.zeros((imgHeight, imgWidth, 4), dtype = "uint8")
+
+    # draw semi-transparent red rectangle
+    overlay[200:300, 0:imgWidth] = (0, 0, 255, 200)
+
+    # Extract the RGB channels
+    srcRGB = Left_alpha[...,:3]
+    dstRGB = Right_alpha[...,:3]
+
+    # Extract the alpha channels and normalise to range 0..1
+    srcA = Left_alpha[...,3]/255.0
+    dstA = Right_alpha[...,3]/255.0
+
+    # Work out resultant alpha channel
+    outA = srcA + dstA*(1-srcA)
+
+    # Work out resultant RGB
+    outRGB = (srcRGB*srcA[...,np.newaxis] + dstRGB*dstA[...,np.newaxis]*(1-srcA[...,np.newaxis])) / outA[...,np.newaxis]
+
+    # Merge RGB and alpha (scaled back up to 0..255) back into single image
+    midline = np.dstack((outRGB,outA*255)).astype(np.uint8)
+
+    # midline = cv2.addWeighted(Right_alpha, glue_alpha, Left_alpha, 1-glue_alpha, 0)
+    # cv2.imwrite( "mask.png", Right_alpha)
+    midline = cv2.cvtColor(midline, cv2.COLOR_RGBA2RGB)
+    # cv2.imshow("midline", midline)
+    
+    cv2.imwrite( "mask.png", midline)
+    filler[:, Left1x.shape[1] - glueing_x + 2 :Left1x.shape[1] - glueing_x + midline.shape[1] - 2] = midline[:,2:-2]
+    # try: 
+    #     stitched = stitch(Left1x,Right1x).copy()
+    # except Exception as e:
+    #     print("Retaking scan")
+    #     take1Xscan()
+    #     Left1x=cv2.imread("data/Left.jpg", cv2.IMREAD_ANYCOLOR) 
+    #     Right1x=cv2.imread("data/Right.jpg", cv2.IMREAD_ANYCOLOR) 
+    #     stitched = stitch(Left1x,Right1x).copy()
+    
+    cv2.imwrite( "stitched.jpg", filler)
+    # stitched = np.uint8(stitched)
+    # height, width = stitched.shape[:-1]   
+    # dst = cv2.cv.CreateMat(height, width, cv2.IMREAD_COLOR)
+    return filler
 
 
 for file in glob.glob("data/scanned/*.jpg"):
